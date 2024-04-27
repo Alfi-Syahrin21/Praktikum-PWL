b6e8dd23c353541e8ffe7da82d9ce806ee2a51e1
GoatMudryk <alfioke7@gmail.com>
2024-04-27 14:47:41 +0700

    Membuat fitur CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Dosen2Controller;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\Dosen2Controller;
 
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +22,11 @@ Route::get('/', function () {
         'isi_data' => 'Halaman Awal'
     ]);
 });
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('index');
+Route::get('/mahasiswa/create', [MahasiswaController::class, 'create'])->name('create');
 Route::get('/dosen', [DosenController::class, 'index']);
 Route::get('/dosen2', [Dosen2Controller::class, 'index']);
-
+Route::post('/mahasiswa/store', [MahasiswaController::class, 'store'])->name('store');
+Route::get('/mahasiswa/edit/{id_mahasiswa}', [MahasiswaController::class, 'edit'])->name('edit');
+Route::put('/mahasiswa/update/{id_mahasiswa}', [MahasiswaController::class, 'update'])->name('update');
+Route::delete('/mahasiswa/{id_mahasiswa}', [MahasiswaController::class, 'destroy'])->name('destroy');
